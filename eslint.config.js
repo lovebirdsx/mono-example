454c9d48fec1ec8410e8fa39178c8e0be532845b
lovebird <lovebirdsx@gmail.com>
2026-04-15 12:34:52 +0800

    优化配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
+import baseConfig from '@acme/config-eslint'
 import reactConfig from '@acme/config-eslint/react'
+
+const withFiles = (configs, files) => configs.map((config) => ({ ...config, files }))
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     ignores: ['**/dist/**', '**/node_modules/**', '**/.turbo/**', '**/*.d.ts'],
   },
-  ...reactConfig,
+  ...withFiles(baseConfig, ['apps/api/**', 'packages/shared/**']),
+  ...withFiles(reactConfig, ['apps/web/**', 'packages/ui/**']),
 ]
